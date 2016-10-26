9575c31cc1e98310dbf3ae134fc9a1603b3e15e8
Brett Walker <bdwalker93@yahoo.com>
2016-10-25 19:45:27 -0700

    github change

--- common/gitconfig.sh
+++ common/gitconfig.sh
@@ -1,8 +1,8 @@
 git config --global core.editor "vim"
 git config --global color.diff true
 git config --global diff.submodule log
-git config --global user.email "keyvanfatehi@gmail.com"
-git config --global user.name "Keyvan Fatehi"
+git config --global user.email "bdwalker93@yahoo.com"
+git config --global user.name "Brett Walker"
 git config --global push.default matching
 git config --global core.excludesfile $DOTCONF/dotfiles/gitignore_global
 echo "git configured per $DOTCONF/gitconfig.sh"
